f982d057abc2510b126587fe281e5835a933b64c
clarkp <clarkpede@gmail.com>
2017-03-07 23:58:55 -0600

    Added basic river generation

--- src/main.cc
+++ src/main.cc
@@ -17,9 +17,95 @@
  */
 
 #include <vector>
-#include "stdlib.h"
-#include "time.h"
+#include <cmath>
+#include <random>
+
 #include "BearLibTerminal.h"
+
+struct Color {
+    int r, g, b;
+    Color(int r, int g, int b) : r(r), g(g), b(b) {};
+};
+
+Color RIVER_COLOR(4,69,143);
+Color BG_COLOR(91,135,20);
+Color BEACH_COLOR(166,157,12);
+
+class River {
+ protected:
+  int length;
+  float min_width = 15;
+  float max_width = 40;
+  float min_travel = 40;
+  float max_travel = 80;
+  int num_periods = 3;
+  std::vector<float> width;
+  std::vector<float> shape;
+  std::mt19937 rng;
+  
+  std::vector<float> RandomSignal(int n, float y_min, float y_max, float min_period, float max_period, int num_periods);
+ public:
+  River(int length);
+  float GetVelocity(int x, int y);
+  bool isBeach(int x, int y);
+};
+
+River::River(int length) : length(length) {
+  width.resize(length);
+  shape.resize(length);
+  rng.seed(std::random_device()());
+  
+  // Create the river
+  width = RandomSignal(length, min_width, max_width, length/2.0, length*2.0, num_periods);
+  shape = RandomSignal(length, min_travel, max_travel, length/3.0, length*2.0, num_periods);
+};
+
+std::vector<float> River::RandomSignal(int n, float y_min, float y_max, 
+                                       float min_period, float max_period, 
+                                       int num_periods) {
+  const float pi = std::atan(1)*4;
+  float min_log = std::log(min_period);
+  float max_log = std::log(max_period);
+  std::vector<float> periods(num_periods);
+  std::vector<float> shifts(num_periods);
+  
+  std::uniform_real_distribution<> dist(0,1);
+  periods[0] = std::exp(dist(rng)*0.25*(max_log - min_log) + min_log);
+  periods[1] = std::exp(dist(rng)*(max_log - min_log) + min_log);
+  periods[2] = std::exp((0.75 + dist(rng)*0.25)*(max_log - min_log) + min_log);
+  for (int i = 0; i<num_periods; i++)
+    shifts[i] = dist(rng)*2.0*pi;
+    
+  std::vector<float> signal(n);
+  for (int i = 0; i<n; i++) {
+    signal[i] = 0.5*(y_max + y_min);
+    for (int j=0; j<num_periods; j++) {
+      signal[i] += 0.5*(y_max-y_min)/num_periods * std::sin(2.0*pi*i/periods[j] + shifts[j]);
+    }
+  }
+  
+  return signal;
+}
+
+float River::GetVelocity(int x, int y) {
+  int i = (int)x;
+  float rescaled = (y - shape[i])/(width[i]/2.0);
+  if (rescaled < 1.0 && rescaled > -1.0) {
+    return 1.0 - rescaled*rescaled;
+  } else {
+    return 0.0;
+  }
+};
+
+bool River::isBeach(int x, int y) {
+  int i = (int)x;
+  float rescaled = std::abs(y - shape[i])/(width[i]/2.0);
+  if (rescaled > 1.0 && rescaled < 1.2) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 class Actor {
  public:
@@ -51,7 +137,8 @@ struct Panel {
 
 struct Tile {
     bool canWalk;
-    Tile() : canWalk(true) {};
+    float vel;
+    Tile() : canWalk(true), vel(0.0) {};
 };
 
 class Map {
@@ -59,6 +146,7 @@ class Map {
   std::vector<Tile> tiles;
   void SetWall(int x, int y);
   bool inBounds(int x, int y) const;
+  River* river;
  public:
   int width, height;
   Position camera;
@@ -70,7 +158,12 @@ class Map {
 
 Map::Map(int width, int height) : width(width), height(height) {
   tiles.resize(height*width);
-  SetWall(20,20);
+  river = new River(width);
+  for (int x=0; x<width; x++) {
+    for (int y=0; y<height; y++) {
+     tiles[x+y*width].vel = river->GetVelocity(x,y);
+    }
+  }
 };
 
 bool Map::isWall(int x, int y) const {
@@ -94,15 +187,21 @@ void Map::Render(Panel panel, Position* camera) const {
     for (int term_y=panel.tl_corner.y; term_y < panel.br_corner.y; term_y++) {
       int game_x = term_x + camera->x - panel.width/2;
       int game_y = height - (term_y + height-camera->y - panel.height/2);
-        if (inBounds(game_x,game_y))
-            terminal_put(term_x, term_y, 
-                         isWall(game_x, game_y)? (int)'#': (int)'.');
+        if (inBounds(game_x,game_y)) {
+          if (tiles[game_x + game_y*width].vel > 0) {
+            terminal_print(term_x, term_y, "[bkcolor=azure]~");
+          } else if (river->isBeach(game_x, game_y)) {
+            terminal_print(term_x, term_y, "[bkcolor=sand].");
+          }
+        }
     }
   }
 };
 
 class Engine {
  protected:
+  const int MAP_WIDTH = 500;
+  const int MAP_HEIGHT = 120;
   int width, height;
   Map* map;
   Panel map_panel;
@@ -123,11 +222,18 @@ class Engine {
 
 Engine::Engine() {
 	terminal_open();
-	terminal_set("window: title='Rogue River: Obol of Charon', resizeable=true, minimum-size=80x24");
+	// Terminal settings
+	terminal_set("window: title='Rogue River: Obol of Charon', resizeable=true, minimum-size=80x24; font: ../graphics/VeraMono.ttf, size=16x16");
+	terminal_bkcolor(color_from_argb(255, BG_COLOR.r, BG_COLOR.g, BG_COLOR.b));
+	terminal_set("palette.sand = #a69d7b");
+	
+	// Initialize engine state
 	width = terminal_state(TK_WIDTH);
     height = terminal_state(TK_HEIGHT);
     status = OPEN;
-    map = new Map(width, height);
+    
+    // Initialize members
+    map = new Map(MAP_WIDTH, MAP_HEIGHT);
 	map_panel.Update(0, 0, width-1, height-1);
 	camera = new Position(width/2, height/2);
     player = new Actor(width/2, height/2, (int)'@');
@@ -138,18 +244,20 @@ Engine::~Engine() {
 };
 
 void Engine::ProcessInput() {
-	int key = terminal_read();
+    while (terminal_has_input()) {
+	    int key = terminal_read();
 
-	if (key == TK_CLOSE || key == TK_ESCAPE) {
-		status = CLOSED;
-	} else if (key == TK_UP) {
-	    camera->y += 1; player->y += 1;
-	} else if (key == TK_DOWN) {
-	    camera->y -= 1; player->y -= 1;
-	} else if (key == TK_LEFT) {
-	    camera->x -= 1; player->x -= 1;
-	} else if (key == TK_RIGHT) {
-	    camera->x += 1; player->x += 1;
+	    if (key == TK_CLOSE || key == TK_ESCAPE) {
+		    status = CLOSED;
+	    } else if (key == TK_UP) {
+	        camera->y += 1; player->y += 1;
+	    } else if (key == TK_DOWN) {
+	        camera->y -= 1; player->y -= 1;
+	    } else if (key == TK_LEFT) {
+	        camera->x -= 1; player->x -= 1;
+	    } else if (key == TK_RIGHT) {
+	        camera->x += 1; player->x += 1;
+	    }
 	}
 };
 
@@ -158,14 +266,7 @@ void Engine::Render() {
 	map->Render(map_panel, camera);
 	terminal_put(player->x - camera->x + map_panel.width/2,
 	             player->y - camera->y + map_panel.height/2, player->symbol);
-	for (int x=0; x<width; x++) {
-		terminal_put(x, 0, x%2? symbol: (int)'#');
-		terminal_put(x, height-1, x%2? symbol: (int)'#');
-	}
-	for (int y=0; y<height; y++) {
-		terminal_put(0, y, y%2? symbol: (int)'#');
-		terminal_put(width-1, y, y%2? symbol: (int)'#');
-	}
+	terminal_printf(map_panel.width/2-5, 0, "X: %d  Y: %d", player->x, player->y);
 	terminal_refresh();
 };
 
